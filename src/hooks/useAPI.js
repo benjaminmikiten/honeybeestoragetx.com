b72d085459916acc02265601ac6f38c7ca52ac77
Benjamin Mikiten <benjaminmikiten@gmail.com>
2020-09-16 11:11:31 -0500

    add error logging, default size

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -3,16 +3,29 @@ import { useState, useEffect } from "react";
 export const useAPI = (url) => {
   const [data, setData] = useState(null);
   const API_URL = "https://api.honeybeestoragetx.com";
+  const TIMEOUT_DURATION = 5 * 1000; //10s
+
+  const requestTimeout = setTimeout(() => {
+    setData({ error: true, message: "Request timed out" });
+    console.error("Request timed out.");
+  }, TIMEOUT_DURATION);
+
   useEffect(() => {
     const fetchData = async () => {
       fetch(`${API_URL}${url}`)
-        .then((response) => response.json())
+        .then((response) => {
+          clearTimeout(requestTimeout);
+          return response.json();
+        })
         .then((result) => {
           console.log("API", result);
           setData(result);
+        })
+        .catch((err) => {
+          setData({ error: true, message: err });
         });
     };
     fetchData();
-  }, [url]);
+  }, [url, requestTimeout]);
   return { data };
 };
